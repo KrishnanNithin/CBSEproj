35fb85d94fe93b610aa114e394e421ef12192bde
KrishnanNithin <75091510+KrishnanNithin@users.noreply.github.com>
2022-07-05 18:20:14 +0400

    add edit functionality

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from playsound import playsound
 
 #variables
 options = ['View records', 'View categories', 'View branches', 'Add record', 'Edit record', 'Delete record']
+options2 = ['Name', 'Category', 'Branch', 'Bill to be paid', 'Amount already paid', 'Notes']
 
 #text to speech
 speak = Dispatch("SAPI.SpVoice").Speak
@@ -137,7 +138,7 @@ def viewbranches(cursor, myconn):
     print('********************************************************************')
     input("Press Enter to continue...")
 
-
+#function edit values
 def editr(cursor, myconn):
     cursor.execute('select * from records;')
     data = cursor.fetchall()
@@ -146,13 +147,45 @@ def editr(cursor, myconn):
     print(records)
     print()
     print('********************************************************************')
-    serial = input('Select the index of the entry to be edited')
-    while True:
-        col = input('Select the attribute to be edited')
-        if col in options:
-            break
-        else:
-            continue
+    serial = input('Select the index of the entry to be edited: ')
+    for i in range(len(options2)):
+        print(f'{i+1}: {options2[i]}')
+    col = int(input('Select the attribute to be edited: '))
+    col -= 1
+    if col in range(len(options2)):
+        newval = input(f'Please enter the new value for the selected column: ')
+        if col == 0:
+            cursor.execute(f'update records set client = "{newval}" where s_no = {int(serial)}')
+            myconn.commit()
+            print('Update completed!')
+            speak('Update completed!')
+        elif col == 1:
+            cursor.execute(f'update records set category_id = {int(newval)} where s_no = {int(serial)}')
+            myconn.commit()
+            print('Update completed!')
+            speak('Update completed!')
+        elif col == 2:
+            cursor.execute(f'update records set branch_id = {int(newval)} where s_no = {int(serial)}')
+            myconn.commit()
+            print('Update completed!')
+            speak('Update completed!')
+        elif col == 3:
+            cursor.execute(f'update records set bill = {int(newval)} where s_no = {int(serial)}')
+            myconn.commit()
+            print('Update completed!')
+            speak('Update completed!')
+        elif col == 4:
+            cursor.execute(f'update records set paid = {int(newval)} where s_no = {int(serial)}')
+            myconn.commit()
+            print('Update completed!')
+            speak('Update completed!')
+        elif col ==5:
+            cursor.execute(f'update records set notes = "{newval}" where s_no = {int(serial)}')
+            myconn.commit()
+            print('Update completed!')
+            speak('Update completed!')
+    else:
+        pass
 
 def remover(cursor, myconn):
     cursor.execute('select * from records;')
